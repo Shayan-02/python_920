1b70be2ec17dfe217e6661b3851763cb9f756568
Shayan Shahbazi <upm200@gmail.com>
2025-02-20 22:47:25 +0330

    j17

---
+++ term02/j17/1.py
@@ -0,0 +1,15 @@
+def sum(a: int, b: int) -> int:
+    """
+    Get tow numbers and return sum of them.
+
+    Args:
+        a (int): first number
+        b (int): second number
+
+    Returns:
+        int: sum of a and b
+    """
+    return a + b
+
+
+sums(10, 20)
